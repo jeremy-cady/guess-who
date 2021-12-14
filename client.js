e077eb120368545abf8bbb0c64366d4c16e3f2bc
jeremy-cady <jeremy.cady@outlook.com>
2021-12-13 20:57:48 -0600

    initial commit

--- client.js
+++ client.js
@@ -1,1 +1,16 @@
+console.log('in client.js');
+
+
 console.log('Here are all the available people:', people);
+
+// incantation
+(document).readyState(onReady);
+
+function onReady() {
+
+}
+
+function displayPeople() {
+
+    
+}
