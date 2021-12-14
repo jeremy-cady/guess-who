315aa6e597e1b7886c54d131c3cca6fafa21c184
jeremy-cady <jeremy.cady@outlook.com>
2021-12-13 22:46:06 -0600

    working on displayPeople function

--- client.js
+++ client.js
@@ -4,13 +4,27 @@ console.log('in client.js');
 console.log('Here are all the available people:', people);
 
 // incantation
-(document).readyState(onReady);
+$(document).ready(onReady);
 
 function onReady() {
-
+    console.log('so ready');
+    
+    displayPeople();
 }
 
 function displayPeople() {
-
+    console.log('in displayPeople');
     
+    for(let i=0; i<people.length; i++) {
+        console.log('in for loop');
+        
+        $('.divsGoHere').append(`
+            <div>
+                <img src="http://github.com/${people.githubUsername}.png?size=250" 
+                    alt="Profile image of ${people.name}"/>
+            </div>
+        `);
+    console.log(`${people.githubUsername}`);
+    
+    }
 }
